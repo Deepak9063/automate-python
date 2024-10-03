5dc11ce4c9d73170b2523318cc26bcd1a2a98556
Deepak999 <deepak.tm@lirisoft.com>
2024-10-03 10:21:22 +0530

    Lambda function

---
+++ .idea/inspectionProfiles/Lambda.py
@@ -0,0 +1,25 @@
+res = (lambda num,p : num**p) (2,3)#usage of lambda function we should not give name for the function
+print(res)                                 # syntax of lambda (lambda arg:expression) (values)
+
+
+def pow(a,b):
+    c=a**b
+    print(c)
+pow(2,3)
+
+
+def get_quotient(num,den):
+    return num/den
+res = get_quotient(10,2)
+print(res)
+res1 = get_quotient(9,3)
+print(res1)
+
+#using lambda function
+fun = lambda num,den : num/den
+res = fun(5,2)
+print(res)
+res1 = fun(9,8)
+print(res1)
+res2 = fun(8,4)
+print(res2)
